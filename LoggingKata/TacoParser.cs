b5c0626467099ba8a4250c4cd58ba9f0cc32ba36
Brian Henderson <henderson.brian82@gmail.com>
2020-09-15 16:09:54 -0400

    Exercise complete

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,4 +1,6 @@
-﻿namespace LoggingKata
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LoggingKata
 {
     /// <summary>
     /// Parses a POI file to locate all the Taco Bells
@@ -6,15 +8,13 @@
     public class TacoParser
     {
         readonly ILog logger = new TacoLogger();
-        
+
         public ITrackable Parse(string line)
         {
             logger.LogInfo("Begin parsing");
 
-            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
             var cells = line.Split(',');
 
-            // If your array.Length is less than 3, something went wrong
             if (cells.Length < 3)
             {
                 // Log that and return null
@@ -22,23 +22,21 @@
                 return null; // TODO Implement
             }
 
-            // grab the latitude from your array at index 0
-            // grab the longitude from your array at index 1
-            // grab the name from your array at index 2
 
-            // Your going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
+            var lat = double.Parse(cells[0]);
 
-            // You'll need to create a TacoBell class
-            // that conforms to ITrackable
+            var lon = double.Parse(cells[1]);
 
-            // Then, you'll need an instance of the TacoBell class
-            // With the name and point set correctly
+            var name = cells[2];
 
-            // Then, return the instance of your TacoBell class
-            // Since it conforms to ITrackable
+            var tacoBell = new TacoBell();
 
-            return null;
+            Point p = new Point();
+            p.Latitude = lat;
+            p.Longitude = lon;
+            tacoBell.Location = p;
+            tacoBell.Name = name;
+            return tacoBell;
         }
     }
 }
